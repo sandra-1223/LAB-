637ef2b03efbfe6666fbe74bb822f6fc06ec0a58
Sandra Sony <89525911+sandra-1223@users.noreply.github.com>
2022-02-09 23:22:40 +0530

    Add files via upload

---
+++ spa.js
@@ -0,0 +1,47 @@
+angular.module('myapp',['ngRoute'])
+.config(function($routeProvider)
+{
+    $routeProvider.when('/home',
+    {
+        templateUrl:'christ/home.html',
+        controller:'homectrl'}).when('/home/:first/:last',
+        {
+            templateUrl:'christ/home.html',
+            controller:'homectrl'
+    }).when('/course',
+    {
+        templateUrl:'christ/course.html',
+        controller:'coursectrl'
+    }).when('/student',
+    {
+        templateUrl:'christ/student.html',
+        controller:'studentctrl'
+    })
+})
+.controller('myctrl',function()
+{
+
+})
+.controller("homectrl",function($scope,$routeParams)
+{
+    $scope.message="Home oooooooooooPage"
+    if($routeParams.first&&$routeParams.last)
+    {
+        $scope.person={
+            first:$routeParams.first,
+            last:$routeParams.last
+        };
+    }
+})
+.controller("coursectrl",function($scope)
+{
+   $scope.courses=["MCA","MSC","MBA","BCA","BCom","BBA"]; 
+})
+.controller("studentctrl",function($scope,$http)
+{
+    $http.get('https://sandra-1223.github.io/LAB-8_Accessing-JSON-file-remotely-using-Angular-JS-with-Filters/data.json') 
+    .success(function(response)
+    {
+        $scope.students=response.records;
+    });
+});
